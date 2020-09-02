f44b273af9f61d5260cdbb83a8a2ab64ed0cc744
Nisha Lathiya <nishalathiya1296@gmail.com>
2020-09-02 18:08:58 +0530

    hello this ia first commit

---
+++ src/app/app.component.ts
@@ -0,0 +1,29 @@
+import { Component } from '@angular/core';
+
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css'],
+
+})
+export class AppComponent { 
+	todoArray=[]
+	addTodo(value){    
+		this.todoArray.push(value)
+		console.log(this.todoArray)    
+	}
+
+	todoSubmit(value:any){     
+		if(value!=="")
+		{    this.todoArray.push(value.todo)}
+		else{      alert('Field required **')    }      
+	}
+
+	deleteItem(todo){   for(let i=0 ;i<= this.todoArray.length ;i++)
+		{    if(todo== this.todoArray[i])
+			{     this.todoArray.splice(i,1)    
+			}   
+		}  
+	} 
+}
